020b1cb38c304bcaefc61b69851e163f8ff8843c
Guillermo Mansilla <mansillag@gmail.com>
2013-11-15 19:16:14 -0500

    adding initial database, default password is 'password'

---
+++ TodoListWeb/dump.sql
@@ -0,0 +1,58 @@
+/*
+ Navicat Premium Data Transfer
+
+ Source Server         : local
+ Source Server Type    : MySQL
+ Source Server Version : 50527
+ Source Host           : localhost
+ Source Database       : todo
+
+ Target Server Type    : MySQL
+ Target Server Version : 50527
+ File Encoding         : utf-8
+
+ Date: 11/15/2013 18:45:44 PM
+*/
+
+SET NAMES utf8;
+SET FOREIGN_KEY_CHECKS = 0;
+
+-- ----------------------------
+--  Table structure for `tasks`
+-- ----------------------------
+DROP TABLE IF EXISTS `tasks`;
+CREATE TABLE `tasks` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `user_id` int(11) NOT NULL,
+  `name` varchar(140) DEFAULT NULL,
+  `description` varchar(1024) DEFAULT NULL,
+  `priority` char(1) DEFAULT NULL,
+  `due_date` datetime DEFAULT NULL,
+  `created` datetime DEFAULT NULL,
+  `status` char(1) DEFAULT NULL,
+  PRIMARY KEY (`id`),
+  KEY `user_id` (`user_id`),
+  CONSTRAINT `fk_user` FOREIGN KEY (`user_id`) REFERENCES `users` (`id`) ON DELETE CASCADE ON UPDATE CASCADE
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+-- ----------------------------
+--  Table structure for `users`
+-- ----------------------------
+DROP TABLE IF EXISTS `users`;
+CREATE TABLE `users` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `username` varchar(50) DEFAULT NULL,
+  `email` varchar(100) DEFAULT NULL,
+  `password` varchar(40) DEFAULT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=4 DEFAULT CHARSET=latin1;
+
+-- ----------------------------
+--  Records of `users`
+-- ----------------------------
+BEGIN;
+INSERT INTO `users` VALUES ('1', 'gmansilla', 'mansillag@gmail.com', '15bc85b25e440729e665d1af560424033d03170a'), ('2', 'rverma', 'rubyverma2080@gmail.com', '15bc85b25e440729e665d1af560424033d03170a'), ('3', 'sraju', 'sonnykr@gmail.com', '15bc85b25e440729e665d1af560424033d03170a');
+COMMIT;
+
+SET FOREIGN_KEY_CHECKS = 1;
+
